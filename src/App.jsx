1ea95db36ea2d0baeb2a8f4459aaa9dd4b1ab533
sajolkk <sajolkar56@gmail.com>
2024-07-18 01:57:29 +0600

    env configuration file name update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import envConfig from './config/envConfig';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+console.log(envConfig.appwriteUrl);
   return (
     <>
       <h1>Blog with appwrite</h1>
---
+++ src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import envConfig from "../config/envConfig";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(envConfig.appwriteUrl)
+      .setProject(envConfig.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        // call another method
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailPasswordSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      throw error;
+      console.log("app write service :: getCurrentUser :: error ", error);
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+        await this.account.deleteSessions();
+    } catch (error) {
+        throw error;
+        console.log("app write service :: logout :: error ", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
